297d54797d4c50256f2df8716940c051fc078cb5
Anmol Verma <anmolkverma.0808@gmail.com>
2024-03-20 19:36:04 +0530

    Project Completed

--- Backend/index.js
+++ Backend/index.js
@@ -2,18 +2,19 @@ import express from 'express';
 import { PORT, mongoDBURL } from './config.js';
 import mongoose from 'mongoose';
 import bookRoutes from './routes/bookRoutes.js';
+import cors from 'cors';
 
 const app = express();
 
 app.use(express.json());
 
-// app.use(cors());
+app.use(cors());
 
-app.use(cors({
-    origin: 'http://localhost:0808',
-    methods: ['GET','POST','PUT','DELETE'],
-    allowedHeaders: ['Content-Type']
-}));
+// app.use(cors({
+//     origin: 'http://localhost:0808',
+//     methods: ['GET','POST','PUT','DELETE'],
+//     allowedHeaders: ['Content-Type']
+// }));
 
 app.get('/',(req,res) => {
     return res.status(200).send("Welcome to BOOK Store.");
